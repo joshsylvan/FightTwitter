b8ab08b193e9b708fd44b993094ff59828f93135
Joshua Gabrel <joshgabrel@live.co.uk>
2017-12-02 13:53:47 +0000

    PLAYER MOVEMENT

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+	Animator anim;
+	Rigidbody2D rb;
+
+	float horizontalAxis;
+	float speed = 1f;
+
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator> ();
+		rb = this.transform.parent.gameObject.GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (XboxCtrlrInput.XCI.GetButtonDown (XboxCtrlrInput.XboxButton.X)) {
+			if (!anim.GetCurrentAnimatorStateInfo (0).IsName ("Slap")) {
+				anim.SetTrigger ("Slap");
+			}
+		}
+
+		if (XboxCtrlrInput.XCI.GetDPad (XboxCtrlrInput.XboxDPad.Left)) {
+			this.transform.localScale = new Vector3 (-1f, 1f, 1);
+			horizontalAxis = -1;
+		} else if (XboxCtrlrInput.XCI.GetDPad (XboxCtrlrInput.XboxDPad.Right)) {
+			this.transform.localScale = new Vector3 (1f, 1f, 1);
+			horizontalAxis = 1;
+		} else {
+			horizontalAxis = 0;
+		}
+	}
+
+	void FixedUpdate(){
+		this.rb.velocity = new Vector2 (horizontalAxis * speed * Time.fixedTime, this.rb.velocity.y);
 	}
 }
