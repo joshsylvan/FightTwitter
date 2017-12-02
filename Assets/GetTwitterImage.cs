fc246e7c4b5144309af85eade5183169c556a97e
Lucas Rijllart <airsha@live.fr>
2017-12-02 15:25:16 +0000

    Added music and TwitterImage getter

---
+++ Assets/GetTwitterImage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetTwitterImage : MonoBehaviour {
+
+	public string url = "https://twitter.com/lucasrijllart/profile_image?size=original";
+	IEnumerator Start() {
+		Texture2D tex;
+		tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+		WWW www = new WWW(url);
+		yield return www;
+		www.LoadImageIntoTexture(tex);
+		GetComponent<Renderer>().material.mainTexture = tex;
+	}
+}
+
