0df13460331a84d881ac2bdd434d453059112f3d
Joshua Gabrel <joshgabrel@live.co.uk>
2017-12-02 18:11:14 +0000

    Menu Transitions

--- Assets/GameplayManager.cs
+++ Assets/GameplayManager.cs
@@ -44,6 +44,8 @@ public class GameplayManager : MonoBehaviour {
 		this.player2.transform.position = new Vector2 (3.6f, 0);
 		gameOver = false;
 		this.cameraFollow.ResetCamera ();
+		//
+
 	}
 
 
---
+++ Assets/PlatformMoving.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMoving : MonoBehaviour {
+
+	GameObject[] platforms;
+
+	int platformID = 0;
+	float randomSeconds = 10;
+
+	bool platformMoving = false;
+
+	Vector2 target, oldPosition;
+
+	// Use this for initialization
+	void Start () {
+		platforms = new GameObject[this.transform.childCount];
+		for (int i = 0; i < this.transform.childCount; i++) {
+			platforms [i] = transform.GetChild (i).gameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		randomSeconds -= Time.deltaTime;
+		if (randomSeconds <= 0 && !platformMoving) {
+			randomSeconds = Random.Range (0, 15);
+			platformID = Random.Range (0, 3);
+			oldPosition = platforms [platformID].transform.position;
+			target = new Vector2 (oldPosition.x, oldPosition.y-2);
+			platformMoving = true;
+		}
+
+		if (platformMoving) {
+
+			Vector2 targetPosition = target;
+
+			Vector2 newPosition = Vector2.Lerp (platforms[platformID].transform.position, targetPosition, 1f * Time.deltaTime);
+
+			this.transform.position = new Vector3 (newPosition.x, newPosition.y, -10);
+		}
+
+	}
+}
