2c4b647593c9c8ba134abc0dfb1f452e71e2246e
Lucas Rijllart <airsha@live.fr>
2017-12-02 17:36:57 +0000

    Changed getTwitterImage

--- Assets/GetTwitterImage.cs
+++ Assets/GetTwitterImage.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GetTwitterImage : MonoBehaviour {
 
-	public string url = "https://twitter.com/lucasrijllart/profile_image?size=original";
-	IEnumerator Start() {
+	public InputField field;
+
+	public IEnumerator LoadPlayerImage(){
+		string username = field.textComponent.text;
+		Debug.Log (username);
 		Texture2D tex;
 		tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
-		WWW www = new WWW(url);
+		WWW www = new WWW("https://twitter.com/"+username+"/profile_image?size=original");
 		yield return www;
 		www.LoadImageIntoTexture(tex);
 		GetComponent<Renderer>().material.mainTexture = tex;
